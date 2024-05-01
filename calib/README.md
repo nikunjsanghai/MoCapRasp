7cfe10e6eefc1e42d04ca630b15cf93a2c841168
adthoms <adthoms@uwaterloo.ca>
2024-04-30 18:09:33 -0700

    update camera calibration for module 3 cameras

--- calib/README.md
+++ calib/README.md
@@ -2,44 +2,30 @@
 
 ## 🏗️ Requirements
 
-- Activate [`libcamera`](https://www.raspberrypi.com/documentation/accessories/camera.html#:~:text=Run%20sudo%20raspi%2Dconfig%20.,Reboot%20your%20Raspberry%20Pi%20again) via `sudo raspi-config` :arrow_right: `interface`
-- Check `picamera` library (default at the Raspbian distro)
+- Deactivate legacy camera support via `sudo raspi-config` :arrow_right: `Interface Options` :arrow_right: `Legacy Camera` :arrow_right: `no`
+- Check `picamera2` library (default at the Raspbian distro)
 ``` python
 python3
-import picamera
-```
-- Create `pics/` folder
-``` bash
-mkdir pics
+import picamera2
 ```
 - Have a bright light source directly behind the camera
-- Print a [calibration pattern](https://docs.opencv.org/4.x/da/d0d/tutorial_camera_calibration_pattern.html) - our has 11 X 12 with 30cm side square ([PDF](https://github.com/debOliveira/myCameraCalibrator/tree/main/python/pdf))
+- Print a [calibration pattern](https://docs.opencv.org/4.x/da/d0d/tutorial_camera_calibration_pattern.html) - ours has 11 X 12 with 30cm side square ([PDF](https://github.com/debOliveira/myCameraCalibrator/tree/main/python/pdf))
 
 ## 🧰 Configuration
 
-### Resolution and sensor mode
+### Gain, Exposure, White Balance
 
-```python
-picamera.PiCamera(resolution=(960,640), framerate=20, sensor_mode=2) # line 7 in calibCapture.py
-```
-- Refer to [sensor mode docs](https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes)
-- Prefer bigger resolutions and modes with full sensor capture
-- Use PNG over compressed types (JPG, bitmap)
-
-### Framerate
-
-- Change the FPS to the lower limit of the sensor mode you choose (see the [table]([url](https://picamera.readthedocs.io/en/release-1.13/fov.html#sensor-modes)) 
-- If you receive pitch-black images, increase the FPS
-
-### Gain
-
-- The gain is set to adjust automatically at the first 5 seconds
+- The gain, exposure, white balance are set to adjust automatically during the first 5 seconds
 - Approximate the calibration pattern in these 5 seconds so the image is very bright
 
 ## ⚔️ Usage
 
-- Run `python3 calibCapture.py`
-- Hit <kbd>Ctrl+C</kbd> to stop
+- Run `python3 calibCapture.py`. For example:
+```sh
+cd ~/MoCapRasp/calib
+python3 calibCapture.py --output_folder=/home/pi/pics
+```
+- Run `python3 calibCapture.py --help` for description of input arguments.
 - Copy the images to the server (use `scp`) and run the [camera calibrator application](https://github.com/debOliveira/myCameraCalibrator)
 
 ## 🖼️ Example pics
--- calib/calibCapture.py
+++ calib/calibCapture.py
@@ -1,26 +1,125 @@
-import time, os
-import picamera
+import os
+import sys
+import time
+import argparse
+import libcamera
+from libcamera import controls
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder, Quality
 
-os.system('rm -rf pics/*')    
-            
-print("[INFO] setting up camera")
-with picamera.PiCamera(resolution=(640,480), framerate=20,
-                       sensor_mode=2) as camera:
-    
-    camera.start_preview(fullscreen=False,window=(100,100,1280,960))
 
-    # Give the camera some warm-up time
+def capture_calibration_image_set(output_folder: str, capture_delay: int, image_dim: tuple, num_images: int) -> None:
+    """
+    Capture a set of images uing the raspberry camera module for calibration
+
+    Args:
+        output_folder: the path to the folder in which image files will be written
+        capture_delay: delay (in seconds) between captured images
+        num_images: the number of image files to capture
+
+    Raises:
+        ValueError: If output_folder does not exist
+        ValueError: If capture_delay is negative
+        ValueError: If num_images is negative
+    """
+
+    # checkout function input
+    if not os.path.isdir(output_folder):
+        raise ValueError(f"Directory {output_folder} does not exist.")
+    else:
+        print(f"[WARNING] {output_folder} does not exist, creating directory")
+        os.system(f"mkdir -p {output_folder}")
+
+    if capture_delay < 0:
+        raise ValueError(f"{capture_delay} must be a positive integer.")
+
+    if num_images < 0:
+        raise ValueError(f"{num_images} must be a positive integer.")
+
+    # remove contents of output folder
+    print(f"Removing contents of {output_folder}")
+    os.system(f"rm -rf {output_folder}/*")
+
+    # set controls to suite camera calibration (see Table 7 of picam2 docs)
+    preview_controls = {
+        "AfMode": controls.AfModeEnum.Manual, "Saturation": 0.0}
+    video_controls = {"AeEnable": False, "AwbEnable": False}
+    video_controls.update(preview_controls)
+
+    # begin capturing
+    print("[INFO] Capturing calibration image set")
+
+    # start preview and set warm-up time for automatically ajusting gain, exposure, white balance, etc.
+    picam2 = Picamera2()
+    picam2.start_preview(Preview.QTGL, transform=libcamera.Transform(hflip=1))
+    picam2.configure(picam2.create_preview_configuration(
+        main={"size": image_dim}, controls=preview_controls))
+    print("[INFO] Starting camera warm-up")
+    picam2.start()
     time.sleep(5)
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    camera.color_effects = (128,128)
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g   
-        
-    print('[RECORDING]')
-    for filename in camera.capture_continuous('pics/img{counter:03d}.jpg'):
-        print('Captured %s' % filename)
-        time.sleep(.5) # Wait 5 minutes
-    camera.stop_preview()
-  
+    picam2.stop()
+    print("[INFO] camera warm-up complete")
+
+    # start video stream
+    picam2.configure(picam2.create_video_configuration(
+        main={"size": image_dim}, controls=video_controls, encode="main"))
+    encoder = H264Encoder(10000000)
+    picam2.start_recording(encoder, output_folder +
+                           "/video_tmp.h264", quality=Quality.HIGH)
+    print("[INFO] Recording started. Capturing still images")
+
+    # capture stills from stream for calibration
+    for image_id in range(num_images):
+        print(dynamic_loading_bar(image_id+1, num_images), end="\r")
+        request = picam2.capture_request()
+        # set ID to be compatible with kalibr_bagcreater
+        request.save("main", output_folder+f"/{image_id}000000000.png")
+        request.release()
+        time.sleep(capture_delay)
+
+    # finish
+    os.system(f"rm -rf {output_folder}/video_tmp.h264")
+    picam2.stop_recording()
+    print("[INFO] Capturing completed. Exiting.")
+
+
+def dynamic_loading_bar(image_id: int, num_images: int) -> None:
+    """
+    Print a dynamic loading bar indicating the number of images captured.
+
+    Args:
+        image_id: the ID of the captured image
+        num_images: the number of image files to capture
+    """
+    bar_length = 40
+    filled_length = int(bar_length * image_id/num_images)
+    bar = "[" + "=" * filled_length + "-" * (bar_length - filled_length) + "]"
+    return f"[INFO] Capturing still images: {bar} image {image_id} of {num_images} captured"
+
+
+def main(args):
+    parser = argparse.ArgumentParser(
+        description='This script is used to collect a set of grey-scale images for camera calibration.')
+
+    parser.add_argument('-o', '--output_folder', type=str,
+                        help='output folder', required=True)
+    parser.add_argument('-d', '--capture_delay', type=int,
+                        help='delay in image capture (sec)', default=1)
+    parser.add_argument('-i', '--image_dim', type=tuple,
+                        help='dimension of image', default=(1536, 864))
+    parser.add_argument('-n', '--num_images', type=int,
+                        help='number of image files to collect ', default=60)
+
+    args = parser.parse_args()
+    output_folder = args.output_folder
+    capture_delay = args.capture_delay
+    image_dim = args.image_dim
+    num_images = args.num_images
+
+    # pair and restamp data/time message couples
+    capture_calibration_image_set(
+        output_folder, capture_delay, image_dim, num_images)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
