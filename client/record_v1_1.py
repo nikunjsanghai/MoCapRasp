16c7e0287643ef6ab096cc46c742a6fb0d574bde
nikunjsanghai <103468688+nikunjsanghai@users.noreply.github.com>
2024-05-14 13:36:22 -0700

    Create record_v1_1.py

---
+++ client/record_v1_1.py
@@ -0,0 +1,76 @@
+import numpy as np
+import picamera
+import picamera.array
+import RPi.GPIO as GPIO
+import time
+import cv2
+import socket
+import argparse
+
+# parser for command line
+parser = argparse.ArgumentParser(description='''Capture client for the MoCap system at the Erobotica lab of UFCG.
+                                                \nPlease use it together with the corresponding server script.''',add_help=False)
+parser.add_argument('-w',type=int,help='image width (default: 960px)',default=960)
+parser.add_argument('-h',type=int,help='image height (default: 720px)',default=720)
+parser.add_argument('-fps',type=int,help='frames per second (default: 40FPS)',default=40)
+parser.add_argument('-ag',type=int,help='camera analog gain (default: 2)',default=2)
+parser.add_argument('-dg',type=int,help='camera digital gain (default: 4)',default=4)
+parser.add_argument('-md',type=int,default=4,help='camera mode (default: 4)')
+parser.add_argument('--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
+args = parser.parse_args()
+frames = [] 
+N_frames = 0
+
+# video capture parameters
+(w,h) = (args.w,args.h)
+md,ag,dg,fps = args.md,args.ag,args.dg,args.fps
+print('[INFO] Size '+str(w)+'x'+str(h)+', FPS '+str(fps)+', mode '+str(md))
+
+# turning LED on
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+led = 4
+GPIO.setup(led,GPIO.OUT)
+print("[INFO] LED on and parameters configured")
+GPIO.output(led,1)
+
+# server parameters
+print('[INFO] connecting to server')
+hostnamePC = socket.gethostbyname('debora-pc.local')
+UDPSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPSocket.sendto(str(str(w)+','+str(h)+','+str(md)).encode(),(hostnamePC, 8888))
+message,_ = UDPSocket.recvfrom(1024)
+start = float(message.split()[0])
+max_frames = int(message.split()[1])*fps
+print('[INFO] waiting trigger')
+now = time.time()
+while now < start: now = time.time()
+print('[INFO] delay in sec: ',now-start)
+
+# running camera capture
+with picamera.PiCamera(resolution=(w, h), framerate=fps) as camera:
+    time.sleep(2)  # warm-up time for the camera
+    start = time.time()
+    stream = picamera.array.PiYUVArray(camera)
+    for _ in camera.capture_continuous(stream, format='yuv', use_video_port=True):
+        # convert YUV to RGB for OpenCV
+        frame = cv2.cvtColor(stream.array, cv2.COLOR_YUV2BGR)
+        ts = time.time()  # capture timestamp
+        # write image
+        cv2.imwrite('/dev/shm/'+str(int(ts * 1e6)).zfill(10)+'.bmp', frame)
+        # free memory
+        stream.seek(0)
+        stream.truncate()
+        N_frames += 1
+        if N_frames == max_frames:
+            break
+
+# closing buffer
+end = time.time()-start
+GPIO.output(led,0)
+print('[INFO] buffer closed and LED off')
+
+# verbose
+elapsed_seconds = float(ts)
+print("[RESULTS] "+str(round(N_frames/elapsed_seconds,2))+" FPS (PTS) and "+
+                   str(round(N_frames/end,2))+" FPS (time lib)")
